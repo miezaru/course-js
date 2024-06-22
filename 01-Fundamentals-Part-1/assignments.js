39ad6a7df8ecdf7e9aef8c31eeb47a6f3532aae0
Artem Usatyi <miezaruexpr@gmail.com>
2024-06-22 22:53:34 +0300

    01 | Values and variables

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,11 @@
+console.log('assignments starts')
+
+let country = 'Ukraine'
+let continent = 'European'
+let population = 38
+
+console.log(country)
+console.log(continent)
+console.log(population)
+
+console.log('assignments end')
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,5 +1,32 @@
 let js = 'amazing'
-// if (js === "amazing") alert("JavaScript is FUN!")
-
-40 + 8 + 23 - 10
 console.log(40 + 8 + 23 - 10)
+
+console.log('Nika')
+console.log(21)
+
+// declaring a variable (a box) in camelCase syntax
+let firstName = 'Nika'
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+// varibales can only contain numbers, letters, undescores or a dollar sign
+// let 3years = 3;
+
+// variable can`t start with a number and use reserve js keywords
+// let function = 27;
+// let name = 'Artem' - name also reserved but allowed to use (in some cases creates problems)
+
+// not starts with uppercase letter, its convention
+// let Person = 'Nika'
+
+// constants in uppercase
+let PI = 3.1415
+
+let myFirstJob = 'Programmer'
+let myCurrentJob = 'Teacher'
+
+let job1 = 'Programmer'
+let job2 = 'Teacher'
+
+console.log(myFirstJob)
