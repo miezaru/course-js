16d5e214808d418316fd433bcf89c612b51cae1d
Artem Usatyi <miezaruexpr@gmail.com>
2024-06-22 23:13:06 +0300

    01 | Data types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -10,23 +10,45 @@ console.log(firstName)
 console.log(firstName)
 console.log(firstName)
 
+//? VARIABLE NAME CONVENTIONS START
 // varibales can only contain numbers, letters, undescores or a dollar sign
-// let 3years = 3;
+// let 3years = 3
 
 // variable can`t start with a number and use reserve js keywords
-// let function = 27;
+// let function = 27
 // let name = 'Artem' - name also reserved but allowed to use (in some cases creates problems)
 
 // not starts with uppercase letter, its convention
 // let Person = 'Nika'
 
 // constants in uppercase
-let PI = 3.1415
+// let PI = 3.1415
 
-let myFirstJob = 'Programmer'
-let myCurrentJob = 'Teacher'
+// let myFirstJob = 'Programmer'
+// let myCurrentJob = 'Teacher'
 
-let job1 = 'Programmer'
-let job2 = 'Teacher'
+// let job1 = 'Programmer'
+// let job2 = 'Teacher'
 
-console.log(myFirstJob)
+// console.log(myFirstJob)
+//? VARIABLE NAME CONVENTIONS END
+
+//* DATA TYPES START
+// typeof
+// console.log(typeof true) // boolean
+// console.log(typeof 2) // number
+// console.log(typeof 'Nika') // string (always use single or double quotes)
+// console.log(typeof undefined) // undefined
+// console.log(typeof null) // object (bug, error), should returned null
+
+let javaScriptIsFun = true
+console.log(javaScriptIsFun, typeof javaScriptIsFun) // true, 'boolean'
+javaScriptIsFun = 'Yes'
+console.log(javaScriptIsFun, typeof javaScriptIsFun) // Yes, string
+
+let year
+console.log(year, typeof year)
+
+year = 1997
+console.log(year, typeof year)
+//* DATA TYPES END
