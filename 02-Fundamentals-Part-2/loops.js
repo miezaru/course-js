e3a6128ab66203765ed66b4a294b75ad1fe07b3f
Artem Usatyi <miezaruexpr@gmail.com>
2024-06-25 20:46:16 +0300

    02 | Iteration: the for loop

---
+++ 02-Fundamentals-Part-2/loops.js
@@ -0,0 +1,23 @@
+'use strict'
+
+//~ ITERATION: THE FOR LOOP START
+/*
+console.log('Lifting weights repetition 1🏋️')
+console.log('Lifting weights repetition 2🏋️')
+console.log('Lifting weights repetition 3🏋️')
+console.log('Lifting weights repetition 4🏋️')
+console.log('Lifting weights repetition 5🏋️')
+console.log('Lifting weights repetition 6🏋️')
+console.log('Lifting weights repetition 7🏋️')
+console.log('Lifting weights repetition 8🏋️')
+console.log('Lifting weights repetition 9🏋️')
+console.log('Lifting weights repetition 10🏋️')
+ */
+
+// for loop needs a starting point, an ending point, and an increment/decrement value
+// for loop keeps running as long as the condition is true
+for (let rep = 1; rep <= 5; rep++) {
+  console.log(`Lifting weights repetition ${rep}🏋️`)
+}
+
+//~ ITERATION: THE FOR LOOP END
