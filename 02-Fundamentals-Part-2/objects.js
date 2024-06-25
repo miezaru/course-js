0824dbad9f46c9ef9b83ecbd44ec03ae792e345d
Artem Usatyi <miezaruexpr@gmail.com>
2024-06-25 18:49:57 +0300

    02 | Introduction to objects

---
+++ 02-Fundamentals-Part-2/objects.js
@@ -0,0 +1,21 @@
+//~ INTRODUCTION TO OBJECTS START
+
+// arrays have no names for elements, only accessed by their index
+const artemArray = ['Artem', 'Usatyi', 2024 - 1997, 'programmer', ['Nika', 'Anna', 'Eva']]
+
+// objects literal syntax creates objects with properties and values
+// order not matter in objects
+const artemObject = {
+  firstName: 'Artem',
+  lastName: 'Usatyi',
+  age: 2024 - 1997,
+  job: 'programmer',
+  friends: ['Nika', 'Anna', 'Eva'],
+}
+
+// use arrays for non structured data
+// use objects for structured data
+
+console.log(artemObject)
+
+//~ INTRODUCTION TO OBJECTS END
