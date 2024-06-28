4ad5570067e4b223a3fbce56e9f2bbebfdf52108
Artem Usatyi <miezaruexpr@gmail.com>
2024-06-28 20:13:28 +0300

    05 | Working with classes

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -5,8 +5,27 @@ const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
 
-console.log(btnsOpenModal)
+// when i need to change styles in javascript better add or remove classes predifined in css file
+
+const openModal = () => {
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+const closeModal = () => {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent)
+  btnsOpenModal[i].addEventListener('click', openModal)
 }
+
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
+
+// for testing purposes
+// const bodyEl = document.querySelector('body')
+// addEventListener('load', () => {
+//   bodyEl.classList.add('hidden')
+// })
