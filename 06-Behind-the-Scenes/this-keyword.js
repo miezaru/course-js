6e2a8a76ec856c3a03b9995d98d521752afd3390
Artem Usatyi <miezaruexpr@gmail.com>
2024-06-30 16:20:26 +0300

    06 | Regular functions vs arrow functions

--- 06-Behind-the-Scenes/this-keyword.js
+++ 06-Behind-the-Scenes/this-keyword.js
@@ -42,4 +42,67 @@ nina.calcAge() // this is points to nina object because nina object IS CALLED a 
 console.log(nina.age, nina)
 
 const f = artem.calcAge // save function to a variable
-f() // not attached to owner
+// f() // not attached to owner
+
+// var firstName = 'Darline'
+
+const henry = {
+  firstName: 'Henry',
+  year: 1994,
+  calcAge: function () {
+    console.log(this)
+    this.age = new Date().getFullYear() - this.year
+
+    //_ How functions in functions using this keyword
+    /* Solution 1
+    const self = this // self or that
+    const isMillenial = function () {
+      console.log(self) // works
+      console.log(self.year >= 1981 && self.year <= 1996)
+      // console.log(this) // undefined
+      // console.log(this.year >= 1981 && this.year <= 1996)
+    }
+    isMillenial() */
+
+    // Solution 2
+    const isMillenial = () => {
+      console.log(this)
+      console.log(this.year >= 1981 && this.year <= 1996)
+
+      const isGood = () => {
+        console.log(`${this.firstName} is good person!`)
+      }
+      isGood()
+    }
+    isMillenial()
+  },
+
+  greet: () => console.log(`Hey ${this.firstName}`),
+}
+// object is not a object scope, but object literal (global scope)
+
+henry.greet() // Darline || undefined
+console.log(this.firstName) // Darline || underfined
+henry.calcAge()
+
+// Not use arrow functions in objects
+
+//_ Arguments keyword (only available in regular functions)
+
+const addExprs = function (a, b) {
+  console.log(arguments) // available in function expressions and declarations (not arrow functions)
+  console.log(...arguments)
+  // return a + b
+  return [...arguments].reduce((acc, curr) => {
+    return acc + curr
+  }, 0)
+}
+addExprs(2, 3)
+console.log(addExprs(2, 5, 8, 12, 33))
+
+var addArrows = (a, b) => {
+  // console.log(arguments) // not defined
+  return a + b
+}
+
+console.log(addArrows(1, 2, 3, 3, 4))
