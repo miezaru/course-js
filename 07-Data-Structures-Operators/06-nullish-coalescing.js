2085053d63a3de3a0dc0e933a72afebc2d4f5de4
Artem Usatyi <miezaruexpr@gmail.com>
2024-07-04 15:52:27 +0300

    07 | The nullish coalescing operator - assignments

--- 07-Data-Structures-Operators/06-nullish-coalescing.js
+++ 07-Data-Structures-Operators/06-nullish-coalescing.js
@@ -60,4 +60,22 @@
   // Nullish values: null and undefined (NOT 0 or '')
   const guestCorrect = restaurant.numGuests ?? 10;
   console.log(guestCorrect);
+
+  //~ Assignments
+
+  const books = [
+    {
+      title: 'Harry Potter and the Chamber of Secrets',
+      onlineContent: false,
+    },
+    {
+      title: 'Jurassic Park',
+    },
+  ];
+
+  // There are objects in the books array that don't have the onlineContent property at all. Loop over the books array, and log a string to the console in this format: "${title}" provides no data about its online content.
+
+  books.forEach(book => {
+    book.onlineContent ?? console.log(`${book.title} provides no data about its online content.`);
+  });
 }
