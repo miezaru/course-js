fd136b815420acd740868fe35d3d7df3e15c153e
Artem Usatyi <miezaruexpr@gmail.com>
2024-07-06 16:59:58 +0300

    07 | Strings, part 1 - assignments

--- 07-Data-Structures-Operators/16-strings.js
+++ 07-Data-Structures-Operators/16-strings.js
@@ -45,4 +45,60 @@
   console.log(new String('artem'));
   console.log(typeof new String('artem')); // object
   console.log(typeof new String('artem').slice()); // string
+
+  //~ Assignments, part 1
+
+  const books = [
+    {
+      title: 'Harry Potter and the Chamber of Secrets',
+      author: ['J.K.Rowling', 'Julie Sussman (Contributor)'],
+      keywords: ['fantasy', 'paranormal'],
+      ISBN: '9780439064866',
+    },
+    {
+      title: 'Jurassic Park',
+      author: 'Stephen King',
+      keywords: ['science-fiction', 'horror'],
+      ISBN: '0345538986',
+    },
+  ];
+
+  // Take the ISBN property of the first book from the books array, and log to the console characters at index 6, 4, 9 and 8. Use bracket notation to access individual characters.
+
+  console.log(books[0].ISBN[6], books[0].ISBN[4], books[0].ISBN[9], books[0].ISBN[8]);
+
+  // Below is the quote variable that stores a string. Find the index of the word 'chess', and log it to the console.
+
+  const quote = 'A computer once beat me at chess, but it was no match for me at kick boxing';
+
+  console.log(quote.indexOf('chess'));
+
+  // Extract the word "boxing" from the same quote string, and log it to the console.
+
+  console.log(quote.slice(quote.indexOf('boxing'), quote.indexOf('boxing') + 'boxing'.length));
+
+  //
+
+  const isContributor = author => {
+    if (typeof author === 'string') {
+      // return console.log(author.lastIndexOf('(Contributor)' || 'contributor') ? true : false);
+      if (author.slice(-'(Contributor)'.length) === '(Contributor)') console.log(author, true);
+      else console.log(author, false);
+    }
+
+    if (typeof author === 'object') {
+      for (const a of author) {
+        if (a.slice(-'(Contributor)'.length) === '(Contributor)') console.log(a, true);
+        else console.log(a, false);
+      }
+    }
+  };
+
+  function isContributor2(author) {
+    return console.log(author.lastIndexOf('(Contributor)') !== -1);
+  }
+
+  isContributor('Julie Sussman (Contributor)');
+  isContributor(books[0].author);
+  isContributor2('Julie Sussman (Contributor)');
 }
