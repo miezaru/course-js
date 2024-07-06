61291ab03f75ebc89d119b7c21b72a468ec6caec
Artem Usatyi <miezaruexpr@gmail.com>
2024-07-06 14:29:05 +0300

    07 | Small fixes

--- 07-Data-Structures-Operators/05-short-circuiting-&&-||.js
+++ 07-Data-Structures-Operators/05-short-circuiting-&&-||.js
@@ -1,116 +1,118 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+{
+  // Data needed for a later exercise
+  const flights =
+    '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  // Data needed for first part of the section
+  const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
+    openingHours: {
+      thu: {
+        open: 12,
+        close: 22,
+      },
+      fri: {
+        open: 11,
+        close: 23,
+      },
+      sat: {
+        open: 0, // Open 24 hours
+        close: 24,
+      },
     },
-    fri: {
-      open: 11,
-      close: 23,
+
+    //_ Destracturing function parameters
+    order: function (starterIndex, mainIndex) {
+      return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
     },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
+
+    //_ Destracturing function parameters
+    orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+      console.log(starterIndex, mainIndex, time, address);
+      console.log(
+        `Order received: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+      );
     },
-  },
 
-  //_ Destracturing function parameters
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
+    //_ Spread operator
+    orderPasta: function (ing1, ing2, ing3) {
+      console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`);
+    },
 
-  //_ Destracturing function parameters
-  orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
-    console.log(starterIndex, mainIndex, time, address);
-    console.log(
-      `Order received: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-    );
-  },
+    //_ Rest operator
+    orderPizza: function (mainIngerient, ...otherIngredients) {
+      console.log(mainIngerient, otherIngredients);
+    },
+  };
 
-  //_ Spread operator
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`);
-  },
+  //~ SHORT CIRCUITING (&& and ||)
 
-  //_ Rest operator
-  orderPizza: function (mainIngerient, ...otherIngredients) {
-    console.log(mainIngerient, otherIngredients);
-  },
-};
+  //_ Use ANY data type, return ANY type, short-circuiting
 
-//~ SHORT CIRCUITING (&& and ||)
+  //_ OR || - first true or (all false) last false values
+  console.log(3 || 'Artem');
+  console.log('' || 'Artem');
+  console.log(true || 0);
+  console.log(undefined || null);
 
-//_ Use ANY data type, return ANY type, short-circuiting
+  console.log(undefined || 0 || '' || 'Hello' || 33 || null);
 
-//_ OR || - first true or (all false) last false values
-console.log(3 || 'Artem');
-console.log('' || 'Artem');
-console.log(true || 0);
-console.log(undefined || null);
+  let guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+  console.log(guests1);
 
-console.log(undefined || 0 || '' || 'Hello' || 33 || null);
+  guests1 = restaurant.numGuests = 23;
+  console.log(guests1);
 
-let guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1);
+  const guests2 = restaurant.numGuests || 10;
+  console.log(guests2);
 
-guests1 = restaurant.numGuests = 23;
-console.log(guests1);
+  //_ AND && - last true or first false returs
+  console.log(0 && 'Artem');
+  console.log(7 && 'Artem');
 
-const guests2 = restaurant.numGuests || 10;
-console.log(guests2);
+  console.log('Hello' && 23 && null && 'Artem');
 
-//_ AND && - last true or first false returs
-console.log(0 && 'Artem');
-console.log(7 && 'Artem');
+  if (restaurant.orderPizza) {
+    restaurant.orderPizza('mushrooms', 'spinach');
+  }
 
-console.log('Hello' && 23 && null && 'Artem');
+  restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
 
-if (restaurant.orderPizza) {
-  restaurant.orderPizza('mushrooms', 'spinach');
+  //~ Assignments
+
+  const books = [
+    {
+      title: 'Harry Potter and the Chamber of Secrets',
+      onlineContent: false,
+    },
+    {
+      title: 'Jurassic Park',
+      onlineContent: true,
+    },
+    {
+      title: 'Algorithms',
+      programmingLanguage: 'Java',
+      onlineContent: true,
+    },
+  ];
+
+  const hasExamplesInJava = function (obj) {
+    return obj.programmingLanguage === 'Java' || 'no data available';
+  };
+
+  console.log(hasExamplesInJava(books[0]));
+  console.log(hasExamplesInJava(books[2]));
+
+  // Some of the book objects have the onlineContent property, which is either true or false. Loop over the books array, and for the books that provide online content, log to the console a string in this format: "${title}" provides online content. Use short-circuiting.
+
+  books.forEach(function (book) {
+    book.onlineContent && console.log(`${book.title} provides online content`);
+  });
 }
-
-restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
-
-//~ Assignments
-
-const books = [
-  {
-    title: 'Harry Potter and the Chamber of Secrets',
-    onlineContent: false,
-  },
-  {
-    title: 'Jurassic Park',
-    onlineContent: true,
-  },
-  {
-    title: 'Algorithms',
-    programmingLanguage: 'Java',
-    onlineContent: true,
-  },
-];
-
-const hasExamplesInJava = function (obj) {
-  return obj.programmingLanguage === 'Java' || 'no data available';
-};
-
-console.log(hasExamplesInJava(books[0]));
-console.log(hasExamplesInJava(books[2]));
-
-// Some of the book objects have the onlineContent property, which is either true or false. Loop over the books array, and for the books that provide online content, log to the console a string in this format: "${title}" provides online content. Use short-circuiting.
-
-books.forEach(function (book) {
-  book.onlineContent && console.log(`${book.title} provides online content`);
-});
--- 07-Data-Structures-Operators/challenge-1.js
+++ 07-Data-Structures-Operators/challenge-1.js
@@ -1,82 +1,84 @@
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
+{
+  const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+      [
+        'Neuer',
+        'Pavard',
+        'Martinez',
+        'Alaba',
+        'Davies',
+        'Kimmich',
+        'Goretzka',
+        'Coman',
+        'Muller',
+        'Gnarby',
+        'Lewandowski',
+      ],
+      [
+        'Burki',
+        'Schulz',
+        'Hummels',
+        'Akanji',
+        'Hakimi',
+        'Weigl',
+        'Witsel',
+        'Hazard',
+        'Brandt',
+        'Sancho',
+        'Gotze',
+      ],
     ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+      team1: 1.33,
+      x: 3.25,
+      team2: 6.5,
+    },
+  };
 
-// 01
-const [players1, players2] = game.players;
-console.log(players1, players2);
+  // 01
+  const [players1, players2] = game.players;
+  console.log(players1, players2);
 
-// 02
-const [gk1, ...fieldPlayers1] = players1;
-const [gk2, ...fieldPlayers2] = players2;
-console.log(gk1, fieldPlayers1);
-console.log(gk2, fieldPlayers2);
+  // 02
+  const [gk1, ...fieldPlayers1] = players1;
+  const [gk2, ...fieldPlayers2] = players2;
+  console.log(gk1, fieldPlayers1);
+  console.log(gk2, fieldPlayers2);
 
-// 03
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
+  // 03
+  const allPlayers = [...players1, ...players2];
+  console.log(allPlayers);
 
-// 04
-const players1Final = [...players1, 'Thiago', 'Couthinho', 'Perisic'];
-console.log(players1Final);
+  // 04
+  const players1Final = [...players1, 'Thiago', 'Couthinho', 'Perisic'];
+  console.log(players1Final);
 
-// 05
-const { team1, x: draw, team2 } = game.odds;
-console.log(team1, draw, team2);
+  // 05
+  const { team1, x: draw, team2 } = game.odds;
+  console.log(team1, draw, team2);
 
-// 06
-const printGoals = function (...players) {
-  console.log(`Total goals: ${game.scored.length}`);
+  // 06
+  const printGoals = function (...players) {
+    console.log(`Total goals: ${game.scored.length}`);
 
-  players = players.filter((player, index) => {
-    return players.indexOf(player) == index;
-  });
+    players = players.filter((player, index) => {
+      return players.indexOf(player) == index;
+    });
 
-  players.forEach(player => {
-    const count = game.scored.filter(x => x == player).length;
+    players.forEach(player => {
+      const count = game.scored.filter(x => x == player).length;
 
-    console.log(`${player}${count ? `, goals: ${count}` : ', no goals'}`);
-  });
-};
-printGoals('Davies', 'Muller', 'Lewandowski', 'Kimnich');
-printGoals(...game.scored);
+      console.log(`${player}${count ? `, goals: ${count}` : ', no goals'}`);
+    });
+  };
+  printGoals('Davies', 'Muller', 'Lewandowski', 'Kimnich');
+  printGoals(...game.scored);
 
-// 07
-team1 < team2 && console.log('Team 1 is more likely to win');
+  // 07
+  team1 < team2 && console.log('Team 1 is more likely to win');
+}
