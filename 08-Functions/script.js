72951759ba953deb37e132b44a4c199b6b1f5eed
Artem Usatyi <miezaruexpr@gmail.com>
2024-07-12 15:27:28 +0300

    08 | Functions, default parameters

--- 08-Functions/script.js
+++ 08-Functions/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+//~ DEFAULT PARAMETERS
+
+const bookings = [];
+
+// Default values are expressions
+// We can use parameters THAT ARE DECLARED before they called
+const createBooking = function (flightNum, numPassangers = 1, price = 199 * numPassangers) {
+  // ES5 (old way)
+  // numPassangers = numPassangers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassangers,
+    price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+// Specify undefined, when we have no values for parameter, it gonna be changed to default value
+createBooking('LH123', undefined, 1000);
