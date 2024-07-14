58874d908983af32aaf0c6a280fa75886297a005
Artem Usatyi <miezaruexpr@gmail.com>
2024-07-14 19:57:24 +0300

    09 | forEach with maps and sets

--- 09-Arrays-Bankist/script.js
+++ 09-Arrays-Bankist/script.js
@@ -91,11 +91,11 @@ const separator = num => {
   if (typeof num === 'string') console.log(`----- ${num} -----`);
 };
 
-const currencies = new Map([
+/* const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
-]);
+]); */
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -213,3 +213,25 @@ movements.forEach((movement, index, array) => {
 // 1: function(450)
 // 2: function(-400)
 // ...
+
+//~ forEach with Maps and Sets
+separator(3);
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+separator('forEach loop on Map');
+currencies.forEach((value, key, map) => {
+  console.log(`${key}: ${value}`);
+});
+
+separator('forEach loop on Set');
+const currenciesUnique = new Set(['USD', 'GBP', 'EUR', 'USD', 'EUR']);
+console.log(currenciesUnique);
+
+currenciesUnique.forEach((value, _, set) => {
+  console.log(`${value}: ${value}`);
+});
