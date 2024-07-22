286106030b561036959fa5ae10d41e8bc1220d35
Artem Usatyi <miezaruexpr@gmail.com>
2024-07-22 12:27:44 +0300

    11 | Event propagation in practice

--- 11-Advanced-DOM-Bankist/script.js
+++ 11-Advanced-DOM-Bankist/script.js
@@ -268,3 +268,37 @@ setTimeout(() => {
 separator(5);
 
 // [img]
+
+//~ Event propagation in practice
+separator(6);
+
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+console.log(randomColor());
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  //_ Stop propagation
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+});
+
+document.querySelector('.nav').addEventListener(
+  'click',
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('NAV', e.target, e.currentTarget);
+    console.log(e.currentTarget === this);
+  },
+  // true - change from bubbling to capturing (false default)
+  false
+);
