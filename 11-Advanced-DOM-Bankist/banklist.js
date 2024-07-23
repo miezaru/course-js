659b3e6d4b2d17e510eebafc5b7bb897a2d44397
Artem Usatyi <miezaruexpr@gmail.com>
2024-07-24 00:40:44 +0300

    11 | Lazy loading images

--- 11-Advanced-DOM-Bankist/banklist.js
+++ 11-Advanced-DOM-Bankist/banklist.js
@@ -237,7 +237,7 @@ headerObserver.observe(headerEl);
 const allSections = document.querySelectorAll('.section');
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
   if (!entry.isIntersecting) return;
 
   entry.target.classList.remove('section--hidden');
@@ -252,3 +252,30 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+//~ Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  imgObserver.unobserve(entry.target); // Remove observer once the image is loaded. This improves performance because it avoids unnecessary checks.
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
