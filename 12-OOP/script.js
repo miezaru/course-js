15182a7714d62ddd334f66ae343bec7029cf3541
Artem Usatyi <miezaruexpr@gmail.com>
2024-07-26 21:32:13 +0300

    12 | Inheritance between classes: constructor functions

--- 12-OOP/script.js
+++ 12-OOP/script.js
@@ -365,3 +365,47 @@ console.log(steven.__proto__ === PersonProto);
 const sarah = Object.create(PersonProto);
 sarah.init('Sarah', 1984);
 console.log(sarah);
+
+//~ Inheritance between "classes": constructor functions
+separator(10);
+/*
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+Person.prototype.calcAge = function () {
+  console.log(new Date().getFullYear() - this.birthYear);
+}; */
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+//_ Linking prototypes
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student('Mike', 2012, 'Computer Science');
+
+console.log(mike);
+mike.introduce();
+
+mike.calcAge();
+
+console.log(mike.__proto__);
+console.log(mike.__proto__.__proto__);
+console.log(mike.__proto__.__proto__.__proto__);
+console.log(mike.__proto__.__proto__.__proto__.__proto__);
+
+console.log(mike instanceof Student);
+console.log(mike instanceof Person);
+console.log(mike instanceof Object);
+
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor);
+
+console.log(mike);
