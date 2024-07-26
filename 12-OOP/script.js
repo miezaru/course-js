7f90c44c988128081628716003cfd7624e7de0c9
Artem Usatyi <miezaruexpr@gmail.com>
2024-07-26 23:45:15 +0300

    12 | Another class example

--- 12-OOP/script.js
+++ 12-OOP/script.js
@@ -476,3 +476,58 @@ console.log(jay);
 
 jay.introduce();
 jay.calcAge2();
+
+//~ Another class example
+separator(13);
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface of our objects (API)
+  deposit(val) {
+    this.movements.push(val);
+    console.log(`${val > 0 ? 'Deposited' : 'Withdrawal'} ${Math.abs(val)} ${this.currency}`);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved: ${val} ${this.currency}`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+console.log(acc1);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+// console.log(acc1);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+console.log(acc1);
+
+console.log(acc1.pin); // can`t be accessible from the user
+
+acc1.requestLoan(1000);
+console.log(acc1.approveLoan()); // can`t be accessible from the user
+
+// Need data encapsulation and data privacy
