ef8a8a71c5725618ba2017fa759f92e8f9e173e6
Artem Usatyi <miezaruexpr@gmail.com>
2024-07-27 20:21:02 +0300

    13 | Using the Geolocation API

--- 13-Mapty/script.js
+++ 13-Mapty/script.js
@@ -1,12 +1,31 @@
-'use strict'
+'use strict';
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form')
-const containerWorkouts = document.querySelector('.workouts')
-const inputType = document.querySelector('.form__input--type')
-const inputDistance = document.querySelector('.form__input--distance')
-const inputDuration = document.querySelector('.form__input--duration')
-const inputCadence = document.querySelector('.form__input--cadence')
-const inputElevation = document.querySelector('.form__input--elevation')
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+//~ How to plan project
+// [img]
+
+//~ Using the Geolocation API
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+
+      //_ Link to my location
+      // console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+}
