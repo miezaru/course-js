15a74fe7bcc46d618a9cf1c2c841c7bedacdc3d9
Artem Usatyi <miezaruexpr@gmail.com>
2024-08-04 01:12:45 +0300

    14 | Other promise combinators: race, allSettled and any

--- 14-Asynchronous/script.js
+++ 14-Asynchronous/script.js
@@ -390,3 +390,51 @@ const get3Countries = async function (c1, c2, c3) {
 };
 
 get3Countries('portugal', 'ukraine', 'poland');
+
+//~ Other promise combinators: race, allSettled and any
+
+// Promise.race - who winning the race
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.com/v3.1/name/italy`),
+    getJSON(`https://restcountries.com/v3.1/name/egypt`),
+    getJSON(`https://restcountries.com/v3.1/name/mexico`),
+  ]);
+  console.log(res[0]);
+})();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(() => {
+      reject(new Error('Request took too long'));
+    }, sec * 1000);
+  });
+};
+
+Promise.race([getJSON(`https://restcountries.com/v3.1/name/tanzania`), timeout(5)]).then(res =>
+  console.log(res[0]).catch(err => console.error(err))
+);
+
+// Promise.allSettled
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+]).then(res => console.log(res));
+
+Promise.all([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+// Promise.any [ES2021] - first fulfilled promise
+Promise.any([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
